535a1b6427460e06e275ecaa55a49e4cd6771cdd
Nguyen Cao Duy <caoduy73@gmail.com>
2020-03-11 20:10:28 +0800

    Update Bipartite.cpp

--- Graph/Bipartite.cpp
+++ Graph/Bipartite.cpp
@@ -51,17 +51,4 @@ int main() {
         int x, y; cin >> x >> y;
         adj[x].push_back(y); adj[y].push_back(x);
     }
-    
-    memset(dist, -1, sizeof dist);
-    Q.push(0); dist[0] = 0;
-    while(!Q.empty()) {
-        int u = Q.front(); Q.pop();
-        for(int i = 0; i < adj[u].size(); i++) {
-            int v = adj[u][i];
-            if(dist[v] == -1) {
-                dist[v] = dist[u] + 1;
-                Q.push(v);
-            }
-        }
-    }
 }
