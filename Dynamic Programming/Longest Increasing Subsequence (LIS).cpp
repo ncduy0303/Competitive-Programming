b28b153b7f7e27facf050741f0ad913957cfa478
Nguyen Cao Duy <caoduy73@gmail.com>
2020-03-11 20:21:31 +0800

    Update Longest Increasing Subsequence (LIS).cpp

--- Dynamic Programming/Longest Increasing Subsequence (LIS).cpp
+++ Dynamic Programming/Longest Increasing Subsequence (LIS).cpp
@@ -16,8 +16,7 @@ typedef vector<vi> vvi;
 #define LSOne(S) (S & (-S))
 #define isBitSet(S, i) ((S >> i) & 1)
 
-int V, E;
-vector<int> adj[MAX_N];
+int N, arr[MAX_N], dp[MAX_N], ans = 0;
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -25,5 +24,26 @@ int main() {
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
   
+    cin >> N;
+    for(int i = 0; i < N; i++) 
+        cin >> arr[i];
     
+    // Solution 1 (using dynamic programming - O(n^2))
+    for (int i = 0; i < N; i++) {
+        dp[i] = 0;
+        for (int j = i - 1; j >= 0; j--) {
+            if (arr[i] > arr[j])
+                dp[i] = max(dp[i], dp[j]);
+        }
+        dp[i]++;
+        ans = max(ans, dp[i]);
+    }
+    
+    //Solution 2 (using binary search - O(nlogn))
+    vector<int> len(N + 1, INF);
+    for (int i = 0; i < N; i++) {
+        int k = lower_bound(len.begin(), len.end(), arr[i]) - len.begin();
+        len[k] = arr[i];
+        ans = max(ans, k);
+    }
 }
