714a795b906ad406d638b0145088f9632e887b59
Nguyen Cao Duy <caoduy73@gmail.com>
2020-03-13 17:34:32 +0800

    Create UFDS.cpp

---
+++ Data Structure/UFDS.cpp
@@ -0,0 +1,16 @@
+// Union-Find Disjoint Set
+// Union by rank & Path compression
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+   
+}
