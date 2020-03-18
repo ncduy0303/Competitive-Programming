dc0e9cd2c1168fc2783e5c312ab2f944b34c8192
Nguyen Cao Duy <caoduy73@gmail.com>
2020-03-18 23:07:13 +0800

    Create Quick_Exponention.cpp

---
+++ Number Theory/Quick_Exponention.cpp
@@ -0,0 +1,36 @@
+// Basically, calculate A^B mod M;
+// Time complexity: O(logB)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 1 << 30;
+const int MAX_N = 100000 + 5;
+const int MAX_L = 20; // ~ Log N
+const long long MOD = 1e9 + 7;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+#define LSOne(S) (S & (-S))
+#define isBitSet(S, i) ((S >> i) & 1)
+
+long long A, B, M;
+
+int qexp(int A, int B, int M) {
+  
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+  
+    cin >> A >> B >> M;
+}
