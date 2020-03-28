c3949797dbcd3d743c50dd73ab33d901714ecbeb
Nguyen Cao Duy <caoduy73@gmail.com>
2020-03-28 15:14:13 +0800

    Update LCA(RMQ).cpp

--- Graph/LCA(RMQ).cpp
+++ Graph/LCA(RMQ).cpp
@@ -1,3 +1,8 @@
+// We can convert the Lowest Common Ancestor (LCA) problem into the Range Minimum Query (RMQ) Problem
+// This can then be solved using common methods such as segment tree or sparse table
+// Details about the algorithm can be found here: 
+// https://www.topcoder.com/community/competitive-programming/tutorials/range-minimum-query-and-lowest-common-ancestor/
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -16,40 +21,76 @@ typedef vector<vi> vvi;
 #define LSOne(S) (S & (-S))
 #define isBitSet(S, i) ((S >> i) & 1)
 
-int N, Q, dp[MAX_N][MAX_L + 1], tin[MAX_N], tout[MAX_N], timer = 0;
+int N, Q, idx = 0;
+int depth[MAX_N * 2], euler[MAX_N * 2], first[MAX_N];
+
+int arr[MAX_N *  2], table[MAX_N * 2][MAX_L + 1], lg[MAX_N * 2 + 1];
+int ST[4 * MAX_N];
+
 bool visited[MAX_N] = {false};
 vector<int> adj[MAX_N];
 
-void dfs(int u, int p) {
+void dfs(int u, int h) {
     if(visited[u]) return;
     visited[u] = true;
     
-    tin[u] = timer++;
-    dp[u][0] = p; // direct parent (2^0 = 1)
+    first[u] = idx;
+    euler[idx] = u;
+    depth[idx++] = h;
     
-    for(int i = 1; i <= MAX_L; i++) 
-        dp[u][i] = dp[dp[u][i - 1]][i - 1];
-    
-    for(int v : adj[u])
-        if(!visited[v])
-            dfs(v, u);
-    
-    tout[u] = timer++;
+    for(int v : adj[u]) {
+        if(!visited[v]) {
+            dfs(v, h + 1);
+            euler[idx] = u;
+            depth[idx++] = h;
+        }
+    }    
 }
 
-bool is_ancestor(int u, int v){ // check if u is an ancestor of v
-    return tin[u] <= tin[v] && tout[u] >= tout[v];
+/* Segment Tree approach
+void build(){
+    for(int i = 2 * N - 1; i > 0; i--){
+        ST[i] = min(ST[i * 2], ST[i * 2 + 1]);
+    }
 }
+
+int RMQ(int l, int r){ //min query in [l..r)
+    int ans = INF;
+    for(l += N * 2, r += N * 2; l < r; l /= 2, r /= 2){
+        if(l % 2 != 0) ans = min(ans, ST[l++]);
+        if(r % 2 != 0) ans = min(ans, ST[--r]);
+    }
+    return ans;
+}
+*/
+
+/* Sparse Table approach
+void build_log_table() { // for O(1) range minimum query
+    lg[1] = 0;
+    for (int i = 2; i <= N; i++)
+        lg[i] = lg[i/2] + 1;
+}
+
+void build_sparse_table() { // O(nlogn)
+    for(int i = 0; i < N; i++)
+        table[i][0] = arr[i];
+
+    for(int j = 1; j <= MAX_L; j++) {
+        for(int i = 0; i + (1 << j) <= N; i++)
+            table[i][j] = min(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
+    }
+}
+
+int RMQ(int l, int r){ // range minimum query in O(1)
+    int j = lg[r - l + 1];
+    return min(table[l][j], table[r - (1 << j) + 1][j]);
+}
+*/
 
 int LCA(int u, int v) {
-    if(is_ancestor(u, v)) return u;
-    if(is_ancestor(v, u)) return v;
-    
-    for (int i = MAX_L; i >= 0; i--)
-        if (!is_ancestor(dp[u][i], v))
-            u = dp[u][i];
-    
-    return dp[u][0];
+    int left = first[u], right = first[v];
+    if (left > right) swap(left, right);
+    return RMQ(left, right);
 }
 
 int main() {
@@ -64,6 +105,24 @@ int main() {
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+    
+    dfs(0, 0);
+    
+    /* Segment Tree approach
+    for(int i = 0; i < N * 2; i++){ //all the elements in the array are assigned as leaves in the ST
+        ST[i + N * 2] = depth[i];
+    }
+    build();
+    */
+    
+    /* Sparse Table approach
+    build_sparse_table();
+    build_log_table();
+    for(int i = 0; i < N * 2; i++){ 
+        arr[i] = depth[i];
+    }
+    */
+    
     while(Q--) {
         int u, v; cin >> u >> v;
         printf("LCA of %d and %d is %d\n", u, v, LCA(u, v));
