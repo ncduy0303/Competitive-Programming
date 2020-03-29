2ab69e68fa8cd7d735cc31a87b0540379be95a4a
Nguyen Cao Duy <caoduy73@gmail.com>
2020-03-29 10:53:28 +0800

    Create 1D Max Sum (Kanade).cpp

---
+++ Dynamic Programming/1D Max Sum (Kanade).cpp
@@ -0,0 +1,30 @@
+// Given an array of integers, find the maximum sum subarray
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 1 << 30;
+const int MAX_N = 1000 + 5;
+const int MAX_L = 20; // ~ Log N
+const long long MOD = 1e9 + 7;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+#define LSOne(S) (S & (-S))
+#define isBitSet(S, i) ((S >> i) & 1)
+
+int N, arr[MAX_N], cur = 0, ans = 0;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+  
+    
+}
