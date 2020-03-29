1776820ec88c28dd92d46d862476ec0f86de22ce
Nguyen Cao Duy <caoduy73@gmail.com>
2020-03-29 10:49:57 +0800

    Create Longest Common Subsequence.cpp

---
+++ Dynamic Programming/Longest Common Subsequence.cpp
@@ -0,0 +1,40 @@
+// Given 2 strings x and y, find the the longest common subsequence
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 1 << 30;
+const int MAX_N = 100000 + 5;
+const int MAX_L = 20; // ~ Log N
+const long long MOD = 1e9 + 7;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+#define LSOne(S) (S & (-S))
+#define isBitSet(S, i) ((S >> i) & 1)
+
+int dp[MAX_N][MAX_N];
+string x, y;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+  
+    cin >> X >> y;
+    memset(dp, 0, sizeof(0));
+    for(int i = 1; i <= x.size(); i++) {
+        for(int j = 1; j <= y.size(); j++) {
+            if(x[i - 1] == y[j - 1]) 
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
+            else
+                dp[i][j] = max(dp[i][j], max(dp[i - 1][j], dp[i][j - 1]));
+        }
+    }
+}
