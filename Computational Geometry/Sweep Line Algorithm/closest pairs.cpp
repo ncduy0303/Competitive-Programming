f08b55d2c3c62d72bae0068672bbb31cdfbb5536
Nguyen Cao Duy <caoduy73@gmail.com>
2020-03-31 11:40:18 +0800

    Update closest pairs.cpp

--- Computational Geometry/Sweep Line Algorithm/closest pairs.cpp
+++ Computational Geometry/Sweep Line Algorithm/closest pairs.cpp
@@ -1,15 +1,14 @@
-// Cutting sticks problem
-// Given a length L and N cutting points, find the minimum cost perform all N cuts
-// The cost of a cut is equal to the length of the stick that we are cutting
-// Example: UVa/10003 https://onlinejudge.org/external/100/10003.pdf
-// Given n points(x, y) on a plane, find the closest distance between a pair of points
+// Given N points (x, y) on a plane, find the minimum distance between a pair of points
+// Idea: Sweep Line Algorithm
+// Reference: https://www.topcoder.com/community/competitive-programming/tutorials/line-sweep-algorithms/
+// Time complexity: (NlogN)
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
 const int INF = 1 << 30;
-const int MAX_N = 100 + 5;
+const int MAX_N = 10000 + 5;
 const int MAX_L = 20; // ~ Log N
 const long long MOD = 1e9 + 7;
 
@@ -37,5 +36,19 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        cin >> pnts[i].second >> pnts[i].first;
+    sort(pnts, pnts + N, cmp);
+
+    box.insert(pnts[0]);
+    int left = 0;
+    for (int i = 1; i < N; i++){
+        while (left < i && pnts[left].second - pnts[i].second > best)
+            box.erase(pnts[left++]);
+        for (auto it = box.lower_bound({pnts[i].second - best, pnts[i].first - best}); it != box.end() && pnts[i].first + best >= it->first; it++)
+            best = min(best, hypot(pnts[i].first - it->first, pnts[i].second - it->second));
+        box.insert(pnts[i]);
+    }
+    cout << best;
 }
