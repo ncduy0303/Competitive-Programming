df324714133ec9c2fbbadba24cb35ac9c6e56ee3
Nguyen Cao Duy <caoduy73@gmail.com>
2020-04-18 15:49:00 +0800

    Update Segment Tree (Non-recursive).cpp

--- Data Structure/Segment Tree/Segment Tree (Non-recursive).cpp
+++ Data Structure/Segment Tree/Segment Tree (Non-recursive).cpp
@@ -20,7 +20,7 @@ typedef vector<vi> vvi;
 #define LSOne(S) (S & (-S))
 #define isBitSet(S, i) ((S >> i) & 1)
 
-int N, Q, arr[MAX_N], st[4 * MAX_N];
+int N, Q, arr[MAX_N], st[2 * MAX_N];
 
 void build(){
     for(int i = N - 1; i > 0; i--){
@@ -30,15 +30,15 @@ void build(){
 
 void update(int i, int val){ //assign val
     for(st[i += N] = val; i > 1; i /= 2){
-        st[i / 2] = st[i] + st[i ^ 2];
+        st[i / 2] = max(st[i], st[i ^ 1]);
     }
 }
 
 int rmq(int l, int r){ //range max query in [l..r)
     int ans = -INF;
-    for(l += N, r += N; l < r; l /= 2, r /= 2){
-        if(l % 2 != 0) ans = max(ans, st[l++]);
-        if(r % 2 != 0) ans = max(ans, st[--r]);
+    for (l += N, r += N; l < r; l /= 2, r /= 2) {
+        if (l % 2 != 0) ans = max(ans, st[l++]);
+        if (r % 2 != 0) ans = max(ans, st[--r]);
     }
     return ans;
 }
@@ -48,15 +48,15 @@ int main() {
     cin.tie(0); cout.tie(0);
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
-    
+
     cin >> N >> Q;
-    for(int i = 0; i < N; i++){ //all the elements in the array are assigned as leaves in the ST
+    for (int i = 0; i < N; i++) { //all the elements in the array are assigned as leaves in the ST
         cin >> st[i + N];
         arr[i] = st[i + N];
     }
     build();
-    while(Q--){
+    while (Q--) { // using 0-indexed
         int i, j; cin >> i >> j;
-        cout << rmq(i, j) << "\n";
+        cout << rmq(i, j + 1) << "\n";
     }
 }
