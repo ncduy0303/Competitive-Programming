ca0f26cab869acbaa4fe0f3d760825bc5917f5f9
Nguyen Cao Duy <caoduy73@gmail.com>
2020-05-20 23:33:51 +0800

    Update Kruskal.cpp

--- Graph/Minimum Spanning Tree/Kruskal.cpp
+++ Graph/Minimum Spanning Tree/Kruskal.cpp
@@ -22,10 +22,11 @@ bool same(int x, int y) {
 void unite(int x, int y) {
     x = root(x), y = root(y);
     num_grp -= same(x, y);
-    if(grpsize[x] > grpsize[y]) {
+    if (grpsize[x] > grpsize[y]) {
         par[y] = x;
         grpsize[x] += grpsize[y];
-    } else {
+    }
+    else {
         par[x] = y;
         grpsize[y] += grpsize[x];
     }
@@ -36,25 +37,25 @@ int main() {
     cin.tie(0); cout.tie(0);
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
-    
+
     cin >> V >> E;
     // Initialize for UFDS
-    for(int i = 0; i < V; ++i) par[i] = i, grpsize[i] = 1;
+    for (int i = 0; i < V; ++i) par[i] = i, grpsize[i] = 1;
     num_grp = V;
-    
-    for(int i = 0; i < E; i++) {
+
+    for (int i = 0; i < E; i++) {
         int u, v, w; cin >> u >> v >> w;
         EdgeList.push_back({w, {u, v}});
     }
     sort(EdgeList.begin(), EdgeList.end());
-    
-    for(int i = 0; i < E; i++) {
+
+    for (int i = 0; i < E; i++) {
         auto edge = EdgeList[i];
         if (!same(edge.second.first, edge.second.second)) {
             mst_cost += edge.first;
             unite(edge.second.first, edge.second.second);
         }
     }
-    
+
     cout << mst_cost;
 }
