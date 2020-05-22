ed360697f22c62cbde2757b5cefb0d3afcdb421b
Nguyen Cao Duy <caoduy73@gmail.com>
2020-05-22 15:11:55 +0800

    Update Trie.cpp

--- Data Structure/Trie.cpp
+++ Data Structure/Trie.cpp
@@ -1,36 +1,73 @@
+/* Trie (prefix tree) data structure
+It is useful for string-related problems where we need to store strings based on their common prefix
+Here we can implement it simply using a 2D array instead of pointers and linked lists
+An example problem: https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ffc7/00000000001d3ff3
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int MAX_N = 1e5 + 5;
-const int MAX_M = 2e6 + 5;
-const int MAX_L = 20; // ~ Log N
-const long long MOD = 1e9 + 7;
-const long long INF = 1e9 + 7;
+// MAX_N here is used as the maximum number of nodes in the trie (aka total number of characters)
+const int MAX_N = 2e6 + 5;
 
 typedef long long ll;
 typedef vector<int> vi;
-typedef pair<int,int> ii;
-typedef vector<ii> vii;
 typedef vector<vi> vvi;
 
 #define LSOne(S) (S & (-S))
 #define isBitSet(S, i) ((S >> i) & 1)
 
-int n, k, c[MAX_M][26], cnt[MAX_M], timer = 0;
-ll ans;
+int N, K, num, ans;
+int trie[MAX_N][26], cnt[MAX_N];
+// initially all numbers in trie are 0 (not allocated)
+// cnt[u] = number of strings that end at node u in the trie => this is to mark the end of a string in the trie
+// num stores the total number of allocated nodes in the trie
 
 void dfs(int u = 0, int d = 0) {
     for (int v = 0; v < 26; v++) {
-        if (c[u][v]) {
-            dfs(c[u][v], d + 1);
-            cnt[u] += cnt[c[u][v]];
+        if (trie[u][v]) {
+            dfs(trie[u][v], d + 1);
+            cnt[u] += cnt[trie[u][v]];
+            // so now cnt[u] stores the total number of strings that has a common prefix from the root to node u
         }
     }
-    while (cnt[u] >= k) {
-        cnt[u] -= k;
+    while (cnt[u] >= K) { // greedily dividing into groups of K
+        cnt[u] -= K;
         ans += d;
     }
+}
+
+void insert(string s) { // insert a string into the trie
+	int u = 0; // start at the root node
+	for (char c : s) {
+		if (!trie[u][c - 'A'])  // if this node has not been allocated
+			trie[u][c - 'A'] = ++num; // then allocate the node and increase num by 1
+		u = trie[u][c - 'A']; // move to the next node
+	}
+	cnt[u]++;
+}
+
+bool search(string s) { // check if a string exist in the trie
+	int u = 0;
+	for (char c : s) {
+        if (!trie[u][c - 'A']) return false;
+        u = trie[u][c - 'A'];
+	}
+	return cnt[u] > 0;
+}
+
+void solve() {
+    ans = 0; num = 0;
+    memset(trie, 0, sizeof trie);
+    memset(cnt, 0, sizeof cnt);
+    cin >> N >> K;
+    for (int i = 0; i < N; i++) {
+        string s; cin >> s;
+        insert(s);
+    }
+    dfs();
+    cout << ans << "\n";
 }
 
 int main() {
@@ -39,18 +76,9 @@ int main() {
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
 
-    cin >> n >> k;
-    for (int i = 0; i < n; i++) {
-        string s; cin >> s;
-        int u = 0;
-        for (char d : s) {
-            if (!c[u][d - 'A']) {
-                c[u][d - 'A'] = ++timer;
-            }
-            u = c[u][d - 'A'];
-        }
-        cnt[u]++;
+    int tc; cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        cout << "Case #" << t  << ": ";
+        solve();
     }
-    dfs();
-    cout << ans;
 }
