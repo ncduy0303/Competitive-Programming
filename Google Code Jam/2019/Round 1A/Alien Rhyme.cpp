638c208960c76bfb06b2766da05b564bd35c8815
Nguyen Cao Duy <caoduy73@gmail.com>
2020-05-22 15:11:57 +0800

    Update Alien Rhyme.cpp

--- Google Code Jam/2019/Round 1A/Alien Rhyme.cpp
+++ Google Code Jam/2019/Round 1A/Alien Rhyme.cpp
@@ -1,65 +1,60 @@
-// Problem 3: Alien Rhyme
-// Idea: Use a prefix tree
+// Problem 2: Alien Rhyme
+// Idea: sort the reversed words alphabetically using a trie and take any two adjacent words with
+// a longest common prefix, pair them, remove them from the list, and repeat
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int MAX_N = 1e6 + 5;
-const int MAX_L = 20; // ~ Log N
-const long long MOD = 1e9 + 7;
-const long long INF = 1e18;
+const int MAX_N = 5e4 + 5;
 
 typedef long long ll;
 typedef vector<int> vi;
-typedef pair<int,int> ii;
-typedef vector<ii> vii;
 typedef vector<vi> vvi;
 
 #define LSOne(S) (S & (-S))
 #define isBitSet(S, i) ((S >> i) & 1)
 
-const int ALPHA = 26;
+int N, num, ans;
+int trie[MAX_N][26], cnt[MAX_N];
+
+void insert(string s) { // insert a string into the trie
+	int u = 0; // start at the root node
+	for (char c : s) {
+		if (!trie[u][c - 'A'])  // if this node has not been allocated
+			trie[u][c - 'A'] = ++num; // then allocate the node and increase num by 1
+		u = trie[u][c - 'A']; // move to the next node
+	}
+	cnt[u]++;
+}
 
 void solve() {
-    int n; cin >> n;
-    vector<vector<int>> trie;
-    trie.emplace_back(ALPHA, -1);
-    vector<int> visited(1, 0), par(1, -1);
-    while (n--) {
-        string str; cin >> str;
-        int cur = 0;
-        for (int i = str.size() - 1; i >= 0; i--) {
-            char c = str[i];
-            int d = (int)(c - 'A');
-            if (trie[cur][d] == -1) {
-                trie[cur][d] = trie.size();
-                trie.emplace_back(ALPHA, -1);
-                visited.push_back(0);
-                par.push_back(cur);
-            }
-            cur = trie[cur][d];
-            visited[cur]++;
-        }
+    ans = 0; num = 0;
+    memset(trie, 0, sizeof trie);
+    memset(cnt, 0, sizeof cnt);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        string s; cin >> s;
+        reverse(s.begin(), s.end());
+        insert(s);
     }
-    int ans = 0;
-    for (int i = trie.size() - 1; i >= 0; i--) {
-        if (visited[i] < 2) continue;
-        ans++;
-        int u = i;
-        while (u != -1) {
-            visited[u] -= 2;
-            u = par[u];
-        }
-    }
-    cout << 2 * ans << "\n";
+	for (int u = num; u > 0; u--) {
+        for (int v = 0; v < 26; v++)
+            if (trie[u][v])
+                cnt[u] += cnt[trie[u][v]];
+		if (cnt[u] >= 2) { // Greadily add them to the pair
+			ans += 2;
+			cnt[u] -= 2;
+		}
+	}
+	cout << ans << "\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
 
     int tc; cin >> tc;
     for (int t = 1; t <= tc; t++) {
