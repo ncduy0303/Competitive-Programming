2e953fbec848d7dd472959f64f0f65740deedb92
Nguyen Cao Duy <caoduy73@gmail.com>
2020-05-24 00:38:36 +0800

    Update Segment Tree (Lazy Propagation).cpp

--- Data Structure/Segment Tree/Segment Tree (Lazy Propagation).cpp
+++ Data Structure/Segment Tree/Segment Tree (Lazy Propagation).cpp
@@ -44,7 +44,7 @@ void down(int node) { // push the lazy value from a node to its children
 }
 
 void update(int node, int start, int end, int l, int r, int val) { // increase all members in [l, r] by val
-    if (r < start || l > end) return;
+    if (r < start || end < l) return;
     if (l <= start && end <= r) {
         st[node].val += val;
         st[node].lazy += val;
@@ -59,12 +59,13 @@ void update(int node, int start, int end, int l, int r, int val) { // increase a
 }
 
 int query(int node, int start, int end, int l, int r) {
-    if (r < start || l > end) return INT_MAX;
+    if (r < start || end < l) return INT_MAX;
     if (l <= start && end <= r) return st[node].val;
-
-    int mid = (start + end) / 2;
-    down(node);
-    return min(query(2 * node, start, mid, l, r), query(2 * node + 1, mid + 1, end, l, r));
+    else {
+        int mid = (start + end) / 2;
+        down(node);
+        return min(query(2 * node, start, mid, l, r), query(2 * node + 1, mid + 1, end, l, r));
+    }
 }
 
 int main() {
@@ -77,8 +78,8 @@ int main() {
     for (int i = 1; i <= N; i++) cin >> arr[i];
     build(1, 1, N);
     while (Q--) {
-        int q; cin >> q;
-        if (q == 1) {
+        char c; cin >> c;
+        if (c == 'U') {
             int l, r, val; cin >> l >> r >> val;
             update(1, 1, N, l, r, val);
         }
@@ -88,3 +89,21 @@ int main() {
         }
     }
 }
+
+/*
+Example input:
+8 6
+1 2 3 4 5 6 7 8
+Q 2 5
+Q 6 6
+U 3 7 2
+Q 2 5
+U 6 6 -1
+Q 6 6
+
+Expected output:
+2
+6
+2
+7
+*/
