8ef59f2d4032649ca7e888b140edfd2227f38e3e
Nguyen Cao Duy <caoduy73@gmail.com>
2020-06-02 16:58:30 +0800

    Update closest pairs.cpp

--- Computational Geometry/Sweep Line Algorithm/closest pairs.cpp
+++ Computational Geometry/Sweep Line Algorithm/closest pairs.cpp
@@ -1,4 +1,6 @@
 // Given N points (x, y) on a plane, find the minimum distance between a pair of points
+// Problem link: UVa 10245 - The Closest Pair Problem
+
 // Idea: Sweep Line Algorithm
 // Reference: https://www.topcoder.com/community/competitive-programming/tutorials/line-sweep-algorithms/
 // Time complexity: (NlogN)
@@ -7,48 +9,69 @@
 
 using namespace std;
 
-const int INF = 1 << 30;
-const int MAX_N = 10000 + 5;
-const int MAX_L = 20; // ~ Log N
-const long long MOD = 1e9 + 7;
+const int MAX_N = 1e4 + 5;
+const double INF = 1e4;
+const double EPS = 1e-9; // may need to use it to avoid floating point error when comparing double
 
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> ii;
-typedef vector<ii> vii;
-typedef vector<vi> vvi;
+struct pt {
+    double x, y;
+    bool operator < (pt a) { // sort based on x
+        return (x == a.x) ? y < a.y : x < a.x;
+    }
+};
 
-#define LSOne(S) (S & (-S))
-#define isBitSet(S, i) ((S >> i) & 1)
+struct cmp { // custom comparator for set (sort based on y)
+    bool operator() (pt a, pt b) {
+        return (a.y == b.y) ? a.x < b.x : a.y < b.y;
+    }
+};
 
 int N;
-pair<ll, ll> pnts[MAX_N]; // <y, x> so that set automatically sorts based on y-coordinates
-set<pair<ll, ll> > box;
-double best = INF;
+pt pnts[MAX_N];
+set<pt, cmp> box;
+double ans;
 
-bool cmp(pair<ll, ll> a, pair<ll, ll> b){
-    return (a.second < b.second);
+void solve() {
+    for (int i = 0; i < N; i++)
+        cin >> pnts[i].x >> pnts[i].y;
+    sort(pnts, pnts + N); // sort bases on x-coordinates (from left to right)
+
+    ans = INF;
+    /* Naive approach O(N^2) -> Searching through all pair of points
+    for (int l = 0; l < N; l++) {
+        for (int r = l + 1; r < N; r++) {
+            if (pnts[r].second - pnts[l].second > ans) break; // this helps speed up the approach
+            ans = min(ans, hypot(pnts[r].first - pnts[l].first, pnts[r].second - pnts[l].second));
+        }
+    }*/
+    box.clear();
+    box.insert(pnts[0]);
+    for (int l = 0, r = 1; r < N; r++) {
+        // removing leftmost points that will not contribute to the minimum distance
+        while (l < r && pnts[r].x - pnts[l].x >= ans)
+            box.erase(pnts[l++]);
+
+        auto lo = box.lower_bound({pnts[r].x, pnts[r].y - ans});
+        auto hi = box.upper_bound({pnts[r].x, pnts[r].y + ans});
+        for (; lo != hi; lo++)
+            ans = min(ans, hypot(pnts[r].x - lo->x, pnts[r].y - lo->y));
+        /* A more succinct way to write
+        for (auto it = box.lower_bound({pnts[r].x - ans, pnts[r].y - ans}); it != box.end() && it->y - pnts[r].y <= ans; it++)
+            ans = min(ans, hypot(pnts[r].x - it->x, pnts[r].y - it->y));
+        */
+        box.insert(pnts[r]);
+    }
+
+    if (ans == INF) cout << "INFINITY\n";
+    else cout << fixed << setprecision(4) << ans << "\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
 
-    cin >> N;
-    for (int i = 0; i < N; i++)
-        cin >> pnts[i].second >> pnts[i].first;
-    sort(pnts, pnts + N, cmp); // sort bases on x-coordinates (from left to right)
-
-    box.insert(pnts[0]);
-    int left = 0;
-    for (int i = 1; i < N; i++){
-        while (left < i && pnts[left].second - pnts[i].second > best)
-            box.erase(pnts[left++]);
-        for (auto it = box.lower_bound({pnts[i].second - best, pnts[i].first - best}); it != box.end() && it->first - pnts[i].first <= best; it++)
-            best = min(best, hypot(pnts[i].first - it->first, pnts[i].second - it->second));
-        box.insert(pnts[i]);
-    }
-    cout << best;
+    while (cin >> N && N)
+        solve();
 }
