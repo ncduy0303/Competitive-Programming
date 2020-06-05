4fa620bdc04f309572bdc316c1234bb0a73db7ca
Nguyen Cao Duy <caoduy73@gmail.com>
2020-06-05 15:24:48 +0800

    Update cp_template.cpp

--- Contest templates/cp_template.cpp
+++ Contest templates/cp_template.cpp
@@ -1,11 +1,16 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 
-const int INF = 1 << 30;
-const int MAX_N = 100000 + 5;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const int MAX_N = 1e5 + 5;
 const int MAX_L = 20; // ~ Log N
 const long long MOD = 1e9 + 7;
+const long long INF = 1e9 + 7;
 
 typedef long long ll;
 typedef vector<int> vi;
@@ -16,10 +21,23 @@ typedef vector<vi> vvi;
 #define LSOne(S) (S & (-S))
 #define isBitSet(S, i) ((S >> i) & 1)
 
+
+
+void solve() {
+
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-}
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
 
+    int tc; cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        //cout << "Case #" << t  << ": ";
+        solve();
+    }
+}
