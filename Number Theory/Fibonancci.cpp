e9c8e36a108315417945697f3eab8c8dbaed7d9a
ncduy0303 <caoduy73@gmail.com>
2020-08-14 15:09:22 +0800

    fix error

--- Number Theory/Fibonancci.cpp
+++ Number Theory/Fibonancci.cpp
@@ -38,7 +38,7 @@ struct matrix {
         // for (int i = 0; i < 2; i++) 
         //     for (int j = 0; j < 2; j++)
         //         for (int k = 0; k < 2; k++)
-        //             (o.m[i][j] += (a.m[i][k] + b.m[k][j])) %= MOD; 
+        //             (o.m[i][j] += (a.m[i][k] * b.m[k][j])) %= MOD; 
         return o;
     }
 };
