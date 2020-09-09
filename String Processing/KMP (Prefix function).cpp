48b1a1edf8a1d23346662ad181580079a036b0a0
ncduy0303 <caoduy73@gmail.com>
2020-09-09 14:56:50 +0800

    recode all string processing

--- String Processing/KMP (Prefix function).cpp
+++ String Processing/KMP (Prefix function).cpp
@@ -1,35 +1,27 @@
-/*
-Given a string S (with length N) and a pattern P (with length M), find all the occurrence of P in S
-Naive Approach: O(NM)
-KMP Time complexity: O(N + M)
-Idea: build the prefix function kmp, kmp[i] = is the maximum length of the proper suffix that is also a prefix of substring [1..i]
-*/
+// Given a string s (with length n) and a pattern p (with length m), find all occurrence of p in s
+// Time complexity: O(n + m)
+// Build the prefix function kmp, kmp[i] = is the maximum length of the proper suffix that is also a prefix of substring [1..i]
+// Problem link: https://cses.fi/problemset/task/1753/
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int INF = 1 << 30;
-const int MAX_N = 100000 + 5;
-const int MAX_L = 20; // ~ Log N
-const long long MOD = 1e9 + 7;
+#define ar array
+#define ll long long
 
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> ii;
-typedef vector<ii> vii;
-typedef vector<vi> vvi;
+const int MAX_N = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
 
-#define LSOne(S) (S & (-S))
-#define isBitSet(S, i) ((S >> i) & 1)
-
-int kmp[MAX_N];
+int kmp[MAX_N], num;
 
 void kmp_preprocess(string P) {
     int i = 0, j = -1; kmp[0] = -1;
-    while (i < P.size()) { // pre-process the pattern string P
-        while (j >= 0 && P[i] != P[j]) j = kmp[j];// if different, reset j using kmp array
-        i++; j++; // if same, advance both pointers
+    while (i < P.size()) { 
+        while (j >= 0 && P[i] != P[j]) j = kmp[j];
+        i++; j++; 
         kmp[i] = j;
     }
 }
@@ -39,20 +31,29 @@ void kmp_search(string S, string P) {
     while (i < S.size()) {
         while (j >= 0 && S[i] != P[j]) j = kmp[j];
         i++; j++;
-        if (j == P.size()) { //find a match
-            printf("P is found at index %d in S\n", i - j);
-            j = kmp[j]; // prepare j for the next possible match
+        if (j == P.size()) { // find a match at index i - j
+            num++; 
+            j = kmp[j]; 
         }
     }
+}
+
+void solve() {
+    string s, p; cin >> s >> p;
+    kmp_preprocess(p);
+    kmp_search(s, p);
+    cout << num << "\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
-    string S, P; cin >> S >> P;
-    kmp_preprocess(P);
-    kmp_search(S, P);
+    int tc; tc = 1;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t  << ": ";
+        solve();
+    }
 }
--- String Processing/Z-algo (Z function).cpp
+++ String Processing/Z-algo (Z function).cpp
@@ -1,35 +1,30 @@
-/*
-Given a string S (with length N) and a pattern P (with length M), find all the occurrence of P in S
-Naive Approach: O(NM)
-Z-algorithm Time complexity: O(N + M)
-Idea: build the Z function, Z[i] = the maximum length of the common prefix of substring [i..n] and the original string [1..n]
-*/
+// Given a string s (with length n) and a pattern p (with length m), find all the occurrence of p in s
+// Time complexity: O(n + m)
+// Build the Z function, Z[i] = the maximum length of the common prefix of substring [i..n] and the original string [1..n]
+// Problem link: https://cses.fi/problemset/task/1753/
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int INF = 1 << 30;
-const int MAX_N = 100000 + 5;
-const int MAX_L = 20; // ~ Log N
-const long long MOD = 1e9 + 7;
+#define ar array
+#define ll long long
 
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> ii;
-typedef vector<ii> vii;
-typedef vector<vi> vvi;
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
 
-#define LSOne(S) (S & (-S))
-#define isBitSet(S, i) ((S >> i) & 1)
+int num;
+vector<int> Z;
 
-vector<int> Z_Func(string T) { // build the Z function of string T
-    vector<int> Z(T.size());
+vector<int> Z_Func(string t) { 
+    vector<int> Z(t.size());
     int l = -1, r = -1;
-    Z[0] = T.size(); // base case
-    for (int i = 1; i < T.size(); i++) {
+    Z[0] = t.size(); 
+    for (int i = 1; i < t.size(); i++) {
         Z[i] = (i >= r) ? 0 : min(r - i, Z[i - l]);
-        while (i + Z[i] < T.size() && T[i + Z[i]] == T[Z[i]]) Z[i]++;
+        while (i + Z[i] < t.size() && t[i + Z[i]] == t[Z[i]]) Z[i]++;
         if (i + Z[i] > r) {
             l = i;
             r = i + Z[i];
@@ -38,20 +33,29 @@ vector<int> Z_Func(string T) { // build the Z function of string T
     return Z;
 }
 
-void Z_Algo(string S, string P) { // use the Z function for string matching
-    string T = P + "#" + S;
-    vector<int> Z = Z_Func(T);
-    for (int i = P.size() + 1; i < T.size(); i++)
-        if (Z[i] == P.size())
-            printf("P is found at index %d in S\n", i - P.size() - 1);
+void Z_Algo(string s, string p) { 
+    string t = p + "#" + s;
+    Z = Z_Func(t);
+    for (int i = p.size() + 1; i < t.size(); i++)
+        if (Z[i] == p.size()) // find a match at index i - p.size() - 1
+            num++;
+}
+
+void solve() {
+    string s, p; cin >> s >> p;
+    Z_Algo(s, p);
+    cout << num << "\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
-    string S, P; cin >> S >> P;
-    Z_Algo(S, P);
+    int tc; tc = 1;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t  << ": ";
+        solve();
+    }
 }
